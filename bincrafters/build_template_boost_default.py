533153fa1189e7cdd3ba1a56686b6cfd2470fa99
Uilian Ries <uilianries@gmail.com>
2018-01-06 07:55:42 -0200

    Forward pure_c for Boost build
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- bincrafters/build_template_boost_default.py
+++ bincrafters/build_template_boost_default.py
@@ -5,10 +5,10 @@ import os
 from bincrafters import build_template_default
 
 
-def get_builder(args=None):
+def get_builder(args=None, pure_c=True):
 
     # Bincrafters default is to upload only when stable, but boost is an exception
     # Empty string allows boost packages upload for testing branch
     os.environ["CONAN_UPLOAD_ONLY_WHEN_STABLE"] = ""
 
-    return build_template_default.get_builder(args)
+    return build_template_default.get_builder(args, pure_c)
