58e0de7fa5f8ade7ae7f869e1bbacb7fcd9601ed
Uilian Ries <uilianries@gmail.com>
2018-01-08 22:59:42 -0200

    Fix bad requirements path
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- setup.py
+++ setup.py
@@ -107,7 +107,8 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        '': ['*.txt', '*.md'],
+        '': ['*.md'],
+        'bincrafters' : ['*.txt'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
