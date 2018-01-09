b0562c703071374c947786d425edcf7c91c6aa54
Uilian Ries <uilianries@gmail.com>
2018-01-08 22:36:01 -0200

    Fix bad requirements path
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=get_requires("requirements.txt"),
+    install_requires=get_requires(os.path.join('bincrafters', 'requirements.txt')),
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
