366ae628ba9cfd789742f3a9b26c193716d7e24f
Jerry W <jerrywiltse@gmail.com>
2018-01-26 15:20:38 -0500

    add boost_* as shared option name

--- bincrafters/build_template_boost_default.py
+++ bincrafters/build_template_boost_default.py
@@ -5,10 +5,10 @@ import os
 from bincrafters import build_template_default
 
 
-def get_builder(args=None, pure_c=True):
+def get_builder(args=None, shared_option_name=None, pure_c=False, dll_with_static_runtime=False):
 
     # Bincrafters default is to upload only when stable, but boost is an exception
     # Empty string allows boost packages upload for testing branch
     os.environ["CONAN_UPLOAD_ONLY_WHEN_STABLE"] = ""
 
-    return build_template_default.get_builder(args, pure_c)
+    return build_template_default.get_builder(args=args, shared_option_name="boost_*:shared", pure_c=pure_c, dll_with_static_runtime=dll_with_static_runtime)
--- bincrafters/build_template_default.py
+++ bincrafters/build_template_default.py
@@ -4,13 +4,14 @@
 from bincrafters import build_shared
 
 
-def get_builder(args=None, pure_c=True):
+def get_builder(args=None, shared_option_name=None, pure_c=False, dll_with_static_runtime=False):
+
     builder = build_shared.get_builder(args)
 
     shared_option_name = None
     if build_shared.is_shared():
         shared_option_name = "%s:shared" % build_shared.get_name_from_recipe()
 
-    builder.add_common_builds(shared_option_name=shared_option_name, pure_c=pure_c)
+    builder.add_common_builds(shared_option_name=shared_option_name, pure_c=pure_c, dll_with_static_runtime=dll_with_static_runtime)
 
     return builder
