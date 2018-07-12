79fc0b3c98728906da22033d9b048de7076138ef
Uilian Ries <uilianries@gmail.com>
2018-07-12 17:50:03 -0300

    Write better info about pure_c [skip ci]
    - Add link to conan-package-tools

--- examples/build_shared_library.py
+++ examples/build_shared_library.py
@@ -5,7 +5,8 @@ from bincrafters import build_template_default
 
 
 if __name__ == "__main__":
-    # Set True when the target is NOT pure C project
+    # If your project is C++, pass the pure_c=False
+    # For more information about: https://github.com/conan-io/conan-package-tools#generating-the-build-configurations-automatically
     pure_c = False
     builder = build_template_default.get_builder(pure_c=pure_c)
     builder.run()
