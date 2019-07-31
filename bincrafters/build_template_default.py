8c19eae813f51d6a4f2ccdaf95e0eaa51d504ff9
Uilian Ries <uilianries@gmail.com>
2019-07-31 16:41:58 -0300

    Dont re-use kwargs for add_common_builds
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- bincrafters/build_template_default.py
+++ bincrafters/build_template_default.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import inspect
-from cpt.packager import ConanMultiPackager
 from bincrafters import build_shared
 
 
@@ -10,6 +8,7 @@ def get_builder(shared_option_name=None,
                 dll_with_static_runtime=False,
                 build_policy=None,
                 cwd=None,
+                reference=None,
                 **kwargs):
     recipe = build_shared.get_recipe_path(cwd)
 
@@ -17,18 +16,10 @@ def get_builder(shared_option_name=None,
     if shared_option_name is None and build_shared.is_shared():
         shared_option_name = "%s:shared" % build_shared.get_name_from_recipe(recipe=recipe)
 
-    # Do not pass kwargs from ConanMultiPackager constructor to add_common_builds
-    attributes = inspect.getfullargspec(ConanMultiPackager.__init__)
-    for attribute in attributes.args:
-        try:
-            kwargs.pop(attribute)
-        except:
-            pass
-
     builder.add_common_builds(
         shared_option_name=shared_option_name,
         pure_c=pure_c,
         dll_with_static_runtime=dll_with_static_runtime,
-        **kwargs)
+        reference=reference)
 
     return builder
