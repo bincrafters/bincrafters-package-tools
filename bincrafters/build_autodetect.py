b2a1506fbc28a4106535d2fc9ae3afc85e79a0be
Croydon <git@cr0ydon.com>
2020-03-07 16:21:55 +0100

    Improve C-only detection
    We rely now on the detection of
    
    del self.settings.compiler.libcxx
    and
    del self.settings.compiler.cppstd
    
    in the recipe instead of analyzing the downloaded source code.
    
    This is much, much, much faster and actually more reliable as source code can have C++ files in e.g. external dependencies or test files which are not part of the build.
    
    This also reflects the current status of the Conan Center hooks.

--- bincrafters/build_autodetect.py
+++ bincrafters/build_autodetect.py
@@ -1,34 +1,15 @@
 import os
 import tempfile
-import contextlib
-import yaml
 import sys
 import subprocess
 
-from bincrafters.build_shared import get_version, get_recipe_path, printer, inspect_value_from_recipe, get_os
+from bincrafters.build_shared import get_recipe_path, printer, inspect_value_from_recipe, get_os
 import bincrafters.build_template_default as build_template_default
 import bincrafters.build_template_header_only as build_template_header_only
 import bincrafters.build_template_installer as build_template_installer
-from conans.util.files import load
-from conans import tools
 
 
-_tmp_dir = tempfile.mkdtemp(prefix="bincrafters-package-tools")
 _recipe_path = os.path.dirname(get_recipe_path())
-
-
-@contextlib.contextmanager
-def chdir(newdir):
-    """ Change directory using locked scope
-
-    :param newdir: Temporary folder to move
-    """
-    old_path = os.getcwd()
-    os.chdir(newdir)
-    try:
-        yield
-    finally:
-        os.chdir(old_path)
 
 
 def _file_contains(file, word):
@@ -66,21 +47,6 @@ def _has_setting(setting_name):
     return False
 
 
-def _get_files_with_extensions(folder, extensions):
-    files = []
-    with tools.chdir(folder):
-        for (root, _, filenames) in os.walk("."):
-            for filename in filenames:
-                for ext in [ext for ext in extensions if ext != ""]:
-                    if filename.endswith(".%s" % ext):
-                        files.append(os.path.join(root, filename))
-                    # Look for possible executables
-                    elif ("" in extensions and "." not in filename
-                          and not filename.endswith(".") and "license" not in filename.lower()):
-                        files.append(os.path.join(root, filename))
-    return files
-
-
 def _flush_output():
     sys.stderr.flush()
     sys.stdout.flush()
@@ -94,40 +60,11 @@ def _is_custom_build_py_existing() -> (bool, str):
     return False, None
 
 
-def _perform_downloads() -> list:
-    conandata_file = load(os.path.join(_recipe_path, "conandata.yml"))
-    conan_data = yaml.safe_load(conandata_file)
+def _is_pure_c():
+    if _recipe_contains("del self.settings.compiler.libcxx") and _recipe_contains("del self.settings.compiler.cppstd"):
+        return True
 
-    version = get_version()
-
-    downloads = conan_data["sources"][version]
-    if isinstance(conan_data["sources"][version], dict):
-        downloads = [conan_data["sources"][version], ]
-
-    i = 0
-    download_directories = []
-    for download in downloads:
-        print("Downloading {}".format(download["url"]))
-        tmp_dir = os.path.join(_tmp_dir, str(i))
-        os.mkdir(tmp_dir)
-        with chdir(tmp_dir):
-            tools.get(**download, retry=2, retry_wait=5)
-        i += 1
-        download_directories.append(tmp_dir)
-
-    return download_directories
-
-
-def _is_pure_c(download_directories):
-    cpp_extensions = ["cc", "cpp", "cxx", "c++m", "cppm", "cxxm", "h++", "hh", "hxx", "hpp"]
-    c_extensions = ["c", "h"]
-
-    for download_dir in download_directories:
-        if _get_files_with_extensions(download_dir, cpp_extensions) or \
-                not _get_files_with_extensions(download_dir, c_extensions):
-            return False
-
-    return True
+    return False
 
 
 def _is_conditional_header_only():
@@ -160,23 +97,21 @@ def run_autodetect():
         subprocess.run(["python",  "{}".format(custom_build_py_path)], check=True)
         return
 
-    download_directories = _perform_downloads()
+    is_installer = _is_installer()
+    printer.print_message("Is the package an installer for executable(s)? {}"
+                          .format(str(is_installer)))
 
-    is_unconditional_header_only = _is_unconditional_header_only()
-    printer.print_message("Is the package header only? {}"
-                          .format(str(is_unconditional_header_only)))
+    if not is_installer:
+        is_unconditional_header_only = _is_unconditional_header_only()
+        printer.print_message("Is the package header only? {}"
+                              .format(str(is_unconditional_header_only)))
 
-    if not is_unconditional_header_only:
-        is_installer = _is_installer()
-        printer.print_message("Is the package an installer for executable(s)? {}"
-                              .format(str(is_installer)))
-
-        if not is_installer:
+        if not is_unconditional_header_only:
             is_conditional_header_only = _is_conditional_header_only()
             printer.print_message("Is the package conditionally header only ('header_only' option)? {}"
                                   .format(str(is_conditional_header_only)))
 
-            is_pure_c = _is_pure_c(download_directories)
+            is_pure_c = _is_pure_c()
             printer.print_message("Is the package C-only? {}".format(str(is_pure_c)))
 
     _flush_output()
