4116d08d8b9fe4f5a2cbc71a0a6011041f455cc4
Croydon <git@cr0ydon.com>
2021-02-04 16:34:40 +0100

    Enable Conan revisions

--- bincrafters/build_paths.py
+++ bincrafters/build_paths.py
@@ -2,3 +2,5 @@ BINCRAFTERS_REPO_URL = "https://api.bintray.com/conan/bincrafters/public-conan"
 BINCRAFTERS_REPO_NAME = "public-conan"
 BINCRAFTERS_LOGIN_USERNAME = "bincrafters-user"
 BINCRAFTERS_USERNAME = "bincrafters"
+
+# TODO: Update those after Bintray's shutdown
