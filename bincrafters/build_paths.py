c355703f8de0e470c5236d2b80632da534e65a51
Croydon <git@cr0ydon.com>
2021-04-14 22:13:43 +0200

    Update default remote URL

--- bincrafters/build_paths.py
+++ bincrafters/build_paths.py
@@ -1,6 +1,5 @@
-BINCRAFTERS_REPO_URL = "https://api.bintray.com/conan/bincrafters/public-conan"
+BINCRAFTERS_REPO_URL = "https://bincrafters.jfrog.io/artifactory/api/conan/public-conan"
 BINCRAFTERS_REPO_NAME = "public-conan"
-BINCRAFTERS_LOGIN_USERNAME = "bincrafters-user"
+BINCRAFTERS_LOGIN_USERNAME = "bincrafters-bot"
 BINCRAFTERS_USERNAME = "bincrafters"
 
-# TODO: Update those after Bintray's shutdown
