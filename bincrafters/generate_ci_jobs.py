523cbfb5c0abca272f89a2e606dbab9368cce96b
Rene Rivera <grafikrobot@gmail.com>
2021-05-04 23:18:28 -0500

    The ulian docker image is also broken.. but differently.

--- bincrafters/generate_ci_jobs.py
+++ bincrafters/generate_ci_jobs.py
@@ -66,7 +66,7 @@ def _get_base_config(recipe_directory: str, platform: str, split_by_build_types:
                 {"name": "CLANG 8", "compiler": "CLANG", "version": "8", "os": "ubuntu-18.04"},
                 {"name": "CLANG 9", "compiler": "CLANG", "version": "9", "os": "ubuntu-18.04"},
                 {"name": "CLANG 10", "compiler": "CLANG", "version": "10", "os": "ubuntu-18.04"},
-                {"name": "CLANG 11", "compiler": "CLANG", "version": "11", "os": "ubuntu-18.04", "dockerImage": "uilianries/clang11-conanfy"},
+                {"name": "CLANG 11", "compiler": "CLANG", "version": "11", "os": "ubuntu-18.04"},
             ]
             if run_macos:
                 matrix["config"] += [
