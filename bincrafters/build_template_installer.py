c954d890f86364d8e11eacdebf7351af0083aece
Croydon <git@cr0ydon.com>
2021-10-03 12:33:20 +0200

    Remove build_template_* files

--- bincrafters/build_template_installer.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from bincrafters import build_shared
-
-
-def get_builder(**kwargs):
-    return build_shared.get_builder(**kwargs)
---
+++ tests/conanfile_installer_only.py
@@ -0,0 +1,53 @@
+from conans import ConanFile, CMake, tools
+import os
+
+required_conan_version = ">=1.33.0"
+
+
+class LibnameConan(ConanFile):
+    name = "libname"
+    version = "1.0.0"
+    description = "Keep it short"
+    topics = ("libname", "logging")
+    url = "https://github.com/bincrafters/community"
+    homepage = "https://github.com/original_author/original_lib"
+    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+
+    settings = "os", "arch", "compiler", "build_type"
+
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
+    _cmake = None
+
+    def package_id(self):
+        del self.info.settings.compiler
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+
+    def _configure_cmake(self):
+        if not self._cmake:
+            self._cmake = CMake(self)
+            self._cmake.definitions["BUILD_TESTS"] = False  # example
+            self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
+        # If so, you can just remove the lines below
+        self.copy(pattern="tool_name", dst="bin", keep_path=False)
+        self.copy(pattern="tool_name.exe", dst="bin", keep_path=False)
+
+    def package_info(self):
+        bindir = os.path.join(self.package_folder, "bin")
+        self.output.info("Appending PATH environment variable: {}".format(bindir))
+        self.env_info.PATH.append(bindir)
