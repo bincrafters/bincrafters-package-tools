d1bccaef3782d9931f396986cd93b4b0b771e878
René Ferdinand Rivera Morell <grafikrobot@gmail.com>
2021-10-09 13:45:03 -0500

    URL change failure comment is obsolete with this change.

--- bincrafters/build_paths.py
+++ bincrafters/build_paths.py
@@ -2,5 +2,3 @@ BINCRAFTERS_REPO_URL = "https://bincrafters.jfrog.io/artifactory/api/conan/publi
 BINCRAFTERS_REPO_NAME = "public-conan"
 BINCRAFTERS_LOGIN_USERNAME = "bincrafters-bot"
 BINCRAFTERS_USERNAME = "bincrafters"
-
-# TODO: Change REPO_URL once it is clear, why it breaks the tests....
